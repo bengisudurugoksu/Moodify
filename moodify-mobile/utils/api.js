87fde18601b3eb1670d171ea899c41e48d9456e4
Zeynep Yiğit <z.yigit2022@gtu.edu.tr>
2025-12-10 21:09:53 +0300

    Change body property to use 'text' instead of 'emotion'

--- moodify-mobile/utils/api.js
+++ moodify-mobile/utils/api.js
@@ -17,7 +17,7 @@ export async function sendMessage(emotion) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        emotion,
+        text: emotion,
       }),
       timeout: CONFIG.API.TIMEOUT,
     });
